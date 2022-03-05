1ed26cfb2494f28a58314793a14d0d07f5962a48
Muhammad Fathan Muthahhari <fathanmuthahhari@gmail.com>
2022-03-05 22:49:04 +0700

    [CHORES] fix comments

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,16 +1,11 @@
 import { mount } from "@vue/test-utils";
 import Index from "../../src/resources/app/pages";
 
-// every test file should includes AT LEAST ONE "describe()", "it()", and "expect()"
+// every test file should include AT LEAST ONE "describe()", "it()", and "expect()"
 describe("displays message", () => {
   // mount() returns a wrapped Vue component we can interact with
   it("should renders", () => {
-    // The component to test
-    const MessageComponent = {
-      template: "<p>{{ msg }}</p>",
-      props: ["msg"],
-    };
-
+    // Mounts the component to be tested
     const wrapper = mount(Index, {});
 
     // Assert the rendered text of the component
