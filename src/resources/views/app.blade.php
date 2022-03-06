b610619d72871943a79d3b82157b3d420b380261
Muhammad Fathan Muthahhari <fathanmuthahhari@gmail.com>
2022-03-06 08:48:16 +0700

    [CHORES] fix js location on templates

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script defer src="{{ mix('js/lms-theme.js') }}"></script>
+    <script defer src="{{ mix('/js/lms-theme.js') }}"></script>
     <link rel="stylesheet" href="{{ mix('css/lms-theme.css') }}"></link>
     <link
     href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900"
