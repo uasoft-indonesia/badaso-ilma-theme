faab2824884600c07c5f187afe4968ed94224c33
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-07 12:23:51 +0700

    [REFACTOR] Create findButton wrapper arrow function

--- tests/unit/JoinClass.spec.js
+++ tests/unit/JoinClass.spec.js
@@ -9,6 +9,7 @@ describe("JoinCoursePage", () => {
             const findTitle = () => wrapper.find('#title');
             const findSubTitle = () => wrapper.find('#sub-title');
             const findForm = () => wrapper.find('#code-form');
+            const findButton = () => wrapper.find('#join-btn');
 
             expect(wrapper.find('#join-card').exists()).toBe(true);
             expect(findTitle().exists()).toBe(true);
@@ -17,9 +18,9 @@ describe("JoinCoursePage", () => {
             expect(findSubTitle().text()).toBe('Class code provided by your teacher');
             expect(findForm().exists()).toBe(true);
             expect(findForm().attributes().label).toBe('Class Code');
-            expect(wrapper.find('#join-btn').exists()).toBe(true);
-            expect(wrapper.find('#join-btn').text()).toBe('Join');
-            expect(wrapper.find('#join-btn').attributes().disabled).toBe('');
+            expect(findButton().exists()).toBe(true);
+            expect(findButton().text()).toBe('Join');
+            expect(findButton().attributes().disabled).toBe('');
         });
     })
 });
