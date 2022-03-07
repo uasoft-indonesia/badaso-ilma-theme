a69153ad37bbe390004252f49ab682826ae9e1e2
Ilma Mannix <ilmamannix2013@gmail.com>
2022-03-07 21:32:25 +0700

    [GREEN] implement title

---
+++ src/resources/app/pages/PeopleInCourse.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container>
+        <v-row class="text-primary text-lg" id="title">
+           Teachers
+        </v-row>
+        <v-row>
+            <v-divider class="bg-primary"></v-divider>
+        </v-row>
+        <v-row class="text-primary text-lg" id="title">
+            Classmates
+        </v-row>
+        <v-row>
+            <v-divider class="bg-primary"></v-divider>
+        </v-row>
+    </v-container>
+</template>
+<script>
+export default {
+  components: {},
+};
+</script>
