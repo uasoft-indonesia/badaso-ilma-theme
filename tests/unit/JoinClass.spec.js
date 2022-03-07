f081f5916cee018a5c54a1e1dade3543163f4c4f
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-07 08:01:23 +0700

    [REFACTOR] Refactor tests for checking subtitle

--- tests/unit/JoinClass.spec.js
+++ tests/unit/JoinClass.spec.js
@@ -4,9 +4,11 @@ import JoinCourse from "../../src/resources/app/pages/course/JoinCourse";
 describe("JoinCoursePage", () => {
     it("should renders", () => {
         const wrapper = mount(JoinCourse, {});
+        const findSubTitle = () => wrapper.find('#sub-title');
 
         expect(wrapper.find('#join-card').exists()).toBe(true);
         expect(wrapper.find('#title').text()).toBe('Enter Code');
-        expect(wrapper.find('#sub-title').text()).toBe('Class code provided by your teacher');
+        expect(findSubTitle().exists()).toBe(true);
+        expect(findSubTitle().text()).toBe('Class code provided by your teacher');
     });
 });
