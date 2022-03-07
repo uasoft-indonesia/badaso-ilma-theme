637ac7c1220d9bf919fb76f783d8b97237ec7058
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-07 11:19:35 +0700

    [RED] Create test to check if join button exists

--- tests/unit/JoinClass.spec.js
+++ tests/unit/JoinClass.spec.js
@@ -5,6 +5,7 @@ describe("JoinCoursePage", () => {
     describe("when loaded", () => {
         it("should renders", () => {
             const wrapper = mount(JoinCourse, {});
+
             const findTitle = () => wrapper.find('#title');
             const findSubTitle = () => wrapper.find('#sub-title');
             const findForm = () => wrapper.find('#code-form');
@@ -16,6 +17,7 @@ describe("JoinCoursePage", () => {
             expect(findSubTitle().text()).toBe('Class code provided by your teacher');
             expect(findForm().exists()).toBe(true);
             expect(findForm().attributes().label).toBe('Class Code');
+            expect(wrapper.find('#join-btn').exists()).toBe(true);
         });
     })
 });
