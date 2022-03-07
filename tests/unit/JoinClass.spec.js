75003f9e3bcdc1d738de3a755ef05ab9e18252e9
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-07 08:19:39 +0700

    [REFACTOR] Group test into

--- tests/unit/JoinClass.spec.js
+++ tests/unit/JoinClass.spec.js
@@ -2,15 +2,17 @@ import { mount } from "@vue/test-utils";
 import JoinCourse from "../../src/resources/app/pages/course/JoinCourse";
 
 describe("JoinCoursePage", () => {
-    it("should renders", () => {
-        const wrapper = mount(JoinCourse, {});
-        const findTitle = () => wrapper.find('#title');
-        const findSubTitle = () => wrapper.find('#sub-title');
+    describe("when loaded", () => {
+        it("should renders", () => {
+            const wrapper = mount(JoinCourse, {});
+            const findTitle = () => wrapper.find('#title');
+            const findSubTitle = () => wrapper.find('#sub-title');
 
-        expect(wrapper.find('#join-card').exists()).toBe(true);
-        expect(findTitle().exists()).toBe(true);
-        expect(findTitle().text()).toBe('Enter Code');
-        expect(findSubTitle().exists()).toBe(true);
-        expect(findSubTitle().text()).toBe('Class code provided by your teacher');
-    });
+            expect(wrapper.find('#join-card').exists()).toBe(true);
+            expect(findTitle().exists()).toBe(true);
+            expect(findTitle().text()).toBe('Enter Code');
+            expect(findSubTitle().exists()).toBe(true);
+            expect(findSubTitle().text()).toBe('Class code provided by your teacher');
+        });
+    })
 });
