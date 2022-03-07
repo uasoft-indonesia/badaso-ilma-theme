b806c185f4139c7ea684e29269cc70105333dbe5
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-07 16:59:31 +0700

    [CHORES] Set base color pallete

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,21 @@
+import Vuetify from 'vuetify/lib'
+
+const vuetify = new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#06BBD3',
+                secondary: '#0596A9',
+                light: '#E6F8FB',
+                black: '#0F0F0F',
+                error: '#FF5252',
+                success: '#46C93A',
+                gray: '#CECECE',
+                textGray: '#AEAEAE',
+                white: '#FEFEFE',
+            },
+        },
+    },
+})
+
+export default vuetify;
