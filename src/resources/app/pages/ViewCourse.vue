22bc3076bc6fa6adf76e851207a079f4e47ee964
Nabila Khansa <nabila.khansa@gmail.com>
2022-03-07 20:34:42 +0700

    [GREEN] implement using course card

---
+++ src/resources/app/pages/ViewCourse.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div
+      class="h-16"
+    ></div>
+    <Card id="course-card"/>
+  </div>
+</template>
+
+<script>
+import Card from "../components/CourseCard.vue";
+
+export default {
+  components: {
+    Card,
+  },
+};
+</script>
