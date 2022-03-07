e1e5c7185d6be095cc8e038537edf2b7ff20d2ec
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-07 11:49:14 +0700

    [RED] Create test if button text is join

--- tests/unit/JoinClass.spec.js
+++ tests/unit/JoinClass.spec.js
@@ -18,6 +18,7 @@ describe("JoinCoursePage", () => {
             expect(findForm().exists()).toBe(true);
             expect(findForm().attributes().label).toBe('Class Code');
             expect(wrapper.find('#join-btn').exists()).toBe(true);
+            expect(wrapper.find('#join-button').text()).toBe('Join');
         });
     })
 });
