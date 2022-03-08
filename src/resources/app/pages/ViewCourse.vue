6a417adbb7a1034f7a3cb0780c01afd7bc929ca7
Nabila Khansa <nabila.khansa@gmail.com>
2022-03-08 13:14:39 +0700

    [GREEN] adjust spacing for flex items

--- src/resources/app/pages/ViewCourse.vue
+++ src/resources/app/pages/ViewCourse.vue
@@ -1,9 +1,43 @@
 <template>
-    <div>
+    <div class="screen mx-16">
       <div
         class="h-16"
       ></div>
-      <Card id="course-card"/>
+      <div
+        class="flex flex-row flex-wrap items-start space-x-10"
+      >
+        <div 
+          class="m-0"
+        />
+        <Card 
+          id="course-card"
+          class="mb-10"
+        />
+        <Card 
+          id="course-card"
+          class="mb-10"
+        />
+        <Card 
+          id="course-card"
+          class="mb-10"
+        />
+        <Card 
+          id="course-card"
+          class="mb-10"
+        />
+        <Card 
+          id="course-card"
+          class="mb-10"
+        />
+        <Card 
+          id="course-card"
+          class="mb-10"
+        />
+        <Card 
+          id="course-card"
+          class="mb-10"
+        />
+      </div>
     </div>
 </template>
 
