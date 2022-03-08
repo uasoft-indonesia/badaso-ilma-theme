5d70925d1ab0b6c3b4140ab5f6cb30a9c3eec275
Ilma Mannix <ilmamannix2013@gmail.com>
2022-03-08 23:40:21 +0700

    [GREEN] implement name and avatar

--- src/resources/app/pages/PeopleInCourse.vue
+++ src/resources/app/pages/PeopleInCourse.vue
@@ -1,16 +1,61 @@
 <template>
     <v-container>
         <v-row class="text-primary text-lg" id="title">
-           Teachers
+            <v-col cols="6">
+                Teachers
+            </v-col>
         </v-row>
         <v-row>
-            <v-divider class="bg-primary"></v-divider>
+            <v-col cols="6">
+                <v-divider class="bg-primary"></v-divider>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="1">
+                <v-avatar class="bg-primary text-white" id="avatar" size="35">N</v-avatar>
+            </v-col>
+            <v-col cols="4">
+                <p class="text-base" id="name" style="display: inline-block;margin-left:-50px;margin-top:5px" >Nofaldi Atmam</p>
+            </v-col>
+            </v-col>
         </v-row>
         <v-row class="text-primary text-lg" id="title">
-            Classmates
+            <v-col cols="6">
+                Classmates
+            </v-col>
         </v-row>
         <v-row>
-            <v-divider class="bg-primary"></v-divider>
+            <v-col cols="6">
+                <v-divider class="bg-primary"></v-divider>
+            </v-col>
+        </v-row>
+        </v-row>
+        <v-row>
+            <v-col cols="1">
+                <v-avatar class="bg-primary text-white" id="avatar" size="35">F</v-avatar>
+            </v-col>
+            <v-col cols="4">
+                <p class="text-base" id="name" style="display: inline-block;margin-left:-50px;margin-top:5px" >Fathan Muttahari</p>
+            </v-col>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="1">
+                <v-avatar class="bg-primary text-white" id="avatar" size="35">I</v-avatar>
+            </v-col>
+            <v-col cols="4">
+                <p class="text-base" id="name" style="display: inline-block;margin-left:-50px;margin-top:5px" >Ilma Alpha Mannix</p>
+            </v-col>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="1">
+                <v-avatar class="bg-primary text-white" id="avatar" size="35">A</v-avatar>
+            </v-col>
+            <v-col cols="4">
+                <p class="text-base" id="name" style="display: inline-block;margin-left:-50px;margin-top:5px" >Aldi Naufal Fitrah</p>
+            </v-col>
+            </v-col>
         </v-row>
     </v-container>
 </template>
