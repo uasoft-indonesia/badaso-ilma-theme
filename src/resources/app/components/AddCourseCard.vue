5c3598f01e303b751d62e5c94b5ae5c1feb2a25f
Nabila Khansa <nabila.khansa@gmail.com>
2022-03-08 15:36:44 +0700

    [GREEN] implements create and join btns

--- src/resources/app/components/AddCourseCard.vue
+++ src/resources/app/components/AddCourseCard.vue
@@ -7,20 +7,36 @@
             class="h-48 bg-light font-semibold text-primary flex flex-col items-center"
         >
             <div 
-            id="add-course-tag"
-            class="text-base mb-5 mt-12"
+                class="text-base mb-5 mt-12"
+                id="add-course-tag"
             >
-            Add Course
+                Add Course
             </div>
-            
+            <div
+                class="space-x-2"
+            >
+                <button 
+                    class="bg-white hover:bg-gray w-20 p-1 border border-gray-400 rounded shadow"
+                    color="error"
+                >
+                    <div class="text-primary font-medium text-sm" id="create-btn">
+                        Create
+                    </div>
+                </button>
+                <button
+                    class="bg-primary hover:bg-secondary w-20 p-1 border border-gray-400 rounded shadow"
+                    elevation="0"
+                >
+                    <div class="text-white font-medium text-sm" id="join-btn">
+                        Join
+                    </div>
+                </button>
+            </div>
         </div>  
     </v-card>
 </template>
 
 <script>
-  import 'vuetify/dist/vuetify.min.css'
-  import vue from 'vue';
-  import vuetify from 'vuetify';
 
   export default {
     data: () => ({
