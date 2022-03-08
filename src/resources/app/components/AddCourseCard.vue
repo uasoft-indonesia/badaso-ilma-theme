c7a021893f9873f276b19a763b8a6c9a8c95791a
Nabila Khansa <nabila.khansa@gmail.com>
2022-03-08 15:10:57 +0700

    [GREEN] init add course card

---
+++ src/resources/app/components/AddCourseCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-card
+      class="mx-auto rounded-xl"
+      width="253"
+    >
+        <div
+            class="h-48 bg-light font-semibold text-primary flex flex-col items-center"
+        >
+            
+        </div>  
+    </v-card>
+</template>
+
+<script>
+  import 'vuetify/dist/vuetify.min.css'
+  import vue from 'vue';
+  import vuetify from 'vuetify';
+
+  export default {
+    data: () => ({
+      show: false,
+    }),
+  }
+</script>
