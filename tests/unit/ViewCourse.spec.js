74f37866fc9aef4b96b01c58d03483978304c125
Nabila Khansa <nabila.khansa@gmail.com>
2022-03-08 15:39:57 +0700

    [RED] create add course card test

--- tests/unit/ViewCourse.spec.js
+++ tests/unit/ViewCourse.spec.js
@@ -10,6 +10,9 @@ describe("CourseCard Component", () => {
 
             const findCard1 = () => wrapper.find('#course-card');
             expect(findCard1().exists()).toBe(true);
+
+            const findCard2 = () => wrapper.find('#add-course-card');
+            expect(findCard2().exists()).toBe(true);
         });
     })
 });
