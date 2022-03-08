0eeec010a86abe9f5f709325e7decbacde7fe388
Nabila Khansa <nabila.khansa@gmail.com>
2022-03-08 15:09:59 +0700

    [RED] create test for add course tag

--- tests/unit/AddCourseCard.spec.js
+++ tests/unit/AddCourseCard.spec.js
@@ -2,11 +2,14 @@ import { mount } from "@vue/test-utils";
 import AddCourseCard from "../../src/resources/app/components/CourseCard";
 
 // every test file should include AT LEAST ONE "describe()", "it()", and "expect()"
-describe("CourseCard Component", () => {
+describe("AddCourseCard Component", () => {
     describe("when loaded", () => {
         it("should renders", () => {
             const wrapper = mount(AddCourseCard, {});
             
+            const findAddCourse = () => wrapper.find('#add-course-tag');
+            expect(findAddCourse().exists()).toBe(true);
+            expect(findAddCourse().text()).toBe('Add Course');
         });
     })
 });
