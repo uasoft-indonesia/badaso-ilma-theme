f630a8fd98be792cd3ee5a58fb438c39780115dd
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-08 09:56:10 +0700

    [REFACTOR] Change indentation format

--- tests/unit/JoinClass.spec.js
+++ tests/unit/JoinClass.spec.js
@@ -1,26 +1,26 @@
-import { mount } from "@vue/test-utils";
+import {mount} from "@vue/test-utils";
 import JoinCourse from "../../src/resources/app/pages/JoinCourse";
 
 describe("JoinCoursePage", () => {
-    describe("when loaded", () => {
-        it("should renders", () => {
-            const wrapper = mount(JoinCourse, {});
+  describe("when loaded", () => {
+    it("should renders", () => {
+      const wrapper = mount(JoinCourse, {});
 
-            const findTitle = () => wrapper.find('#title');
-            const findSubTitle = () => wrapper.find('#sub-title');
-            const findForm = () => wrapper.find('#code-form');
-            const findButton = () => wrapper.find('#join-btn');
+      const findTitle = () => wrapper.find('#title');
+      const findSubTitle = () => wrapper.find('#sub-title');
+      const findForm = () => wrapper.find('#code-form');
+      const findButton = () => wrapper.find('#join-btn');
 
-            expect(wrapper.find('#join-card').exists()).toBe(true);
-            expect(findTitle().exists()).toBe(true);
-            expect(findTitle().text()).toBe('Enter Code');
-            expect(findSubTitle().exists()).toBe(true);
-            expect(findSubTitle().text()).toBe('Class code provided by your teacher');
-            expect(findForm().exists()).toBe(true);
-            expect(findForm().attributes().label).toBe('Class Code');
-            expect(findButton().exists()).toBe(true);
-            expect(findButton().text()).toBe('Join');
-            expect(findButton().attributes().disabled).toBe("true");
-        });
-    })
+      expect(wrapper.find('#join-card').exists()).toBe(true);
+      expect(findTitle().exists()).toBe(true);
+      expect(findTitle().text()).toBe('Enter Code');
+      expect(findSubTitle().exists()).toBe(true);
+      expect(findSubTitle().text()).toBe('Class code provided by your teacher');
+      expect(findForm().exists()).toBe(true);
+      expect(findForm().attributes().label).toBe('Class Code');
+      expect(findButton().exists()).toBe(true);
+      expect(findButton().text()).toBe('Join');
+      expect(findButton().attributes().disabled).toBe("true");
+    });
+  });
 });
