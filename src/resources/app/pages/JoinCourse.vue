ee3f0c58120a61c5a7d4728de58a3b076ddecb7c
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-08 07:04:20 +0700

    [REFACTOR] Change indentation formatting

--- src/resources/app/pages/JoinCourse.vue
+++ src/resources/app/pages/JoinCourse.vue
@@ -1,44 +1,44 @@
 <template>
-    <div>
-        <v-card
-            id="join-card"
-            elevation="2"
-            max-width="372px"
-            class="p-6"
+  <div>
+    <v-card
+      id="join-card"
+      elevation="2"
+      max-width="372px"
+      class="p-6"
+    >
+      <div
+        id="title"
+      >
+        Enter Code
+      </div>
+      <div
+        id="sub-title"
+      >
+        Class code provided by your teacher
+      </div>
+      <div>
+        <v-text-field
+          id="code-form"
+          label="Class Code"
+          outlined
+        ></v-text-field>
+      </div>
+      <div>
+        <v-btn
+          id="join-btn"
+          depressed
+          color="#FEFEFE"
+          :disabled=true
         >
-            <div
-                id="title"
-            >
-                Enter Code
-            </div>
-            <div
-                id="sub-title"
-            >
-                Class code provided by your teacher
-            </div>
-            <div>
-                <v-text-field
-                    id="code-form"
-                    label="Class Code"
-                    outlined
-                ></v-text-field>
-            </div>
-            <div>
-                <v-btn
-                    id="join-btn"
-                    depressed
-                    color="#FEFEFE"
-                    :disabled=true
-                >
-                    Join
-                </v-btn>
-            </div>
-        </v-card>
-    </div>
+          Join
+        </v-btn>
+      </div>
+    </v-card>
+  </div>
 </template>
 
 <script>
 export default {
-    components: {},
+  components: {},
 };
 </script>
