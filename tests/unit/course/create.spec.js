eca0a784638d573d6a47af7ccf07c265ffaefdff
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-03-13 09:23:54 +0700

    [RED] test create course rendered the form

---
+++ tests/unit/course/create.spec.js
@@ -0,0 +1,27 @@
+import Vuetify from "vuetify";
+import { mount, createLocalVue } from "@vue/test-utils";
+
+import CreateCourse from "  ../../../src/resources/app/pages/course/CreateCourse";
+
+describe("CreateCourse.vue", () => {
+  let vuetify;
+  const localVue = createLocalVue();
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+  });
+
+  it("should render correct forms", () => {
+    const wrapper = mount(CreateCourse, {
+      localVue,
+      vuetify,
+    });
+
+    expect(wrapper.find("#title").text()).toBeTruthy();
+    expect(wrapper.find("#name-form").exists()).toBe(true);
+    expect(wrapper.find("#subject-form").exists()).toBe(true);
+    expect(wrapper.find("#room-form").exists()).toBe(true);
+    expect(wrapper.find("#cancel-btn").exists()).toBe(true);
+    expect(wrapper.find("#create-btn").exists()).toBe(true);
+  });
+});
