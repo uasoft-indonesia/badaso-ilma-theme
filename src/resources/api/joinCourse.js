b306f919b6c859726a05e4b8fd2e256deee0da46
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-13 21:03:36 +0700

    [CHORES] Change api implementation

--- src/resources/api/joinCourse.js
+++ src/resources/api/joinCourse.js
@@ -3,15 +3,23 @@ import api from "./config";
 let apiPrefix = "/badaso-api";
 
 export default {
-  async joinCourse(data) {
-    let ep = apiPrefix + "/module/lms/v1/course/join";
-    let response = await api.post(ep, data);
+  async joinCourse(payload) {
+    const ep = apiPrefix + "/module/lms/v1/course/join";
 
-      if (response.data) {
-        alert('successful');
-      } else {
-        alert('fail');
+    let [data, error, errorMessage] = [null, null, null];
+    try {
+      const response = await api.post(ep, payload);
+
+      if (!!response.errors) {
+        throw response.errors;
       }
-    return response;
+
+      data = payload.data;
+    } catch (e) {
+      error = e;
+      errorMessage = "Something went wrong, please try again later";
+    }
+
+    return { data, error, errorMessage }
   }
 }
