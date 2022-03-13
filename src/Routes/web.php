6229c68797c4eda58992688e905947f87944831c
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-13 22:17:58 +0700

    [CHORES] Change routing

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -13,13 +13,14 @@ Route::prefix($lms_route_prefix)
         Route::get('/', [InertiaController::class, 'home']);
 
         /* course routes */
-        Route::get('/join', [InertiaController::class, 'joinCourse']);
         Route::get('/register', [InertiaController::class, 'register']);
         Route::get('/landing', [InertiaController::class, 'view']);
+        Route::get('/login', [InertiaController::class, 'login']);
 
         Route::prefix('/course')->as('course.')->group(function () {
             Route::get('/create', [InertiaController::class, 'createCourse']);
             Route::get('/{id}/people', [InertiaController::class, 'people']);
-            Route::get('/login', [InertiaController::class, 'login']);
+            Route::get('/join', [InertiaController::class, 'joinCourse']);
+
         });
     });
