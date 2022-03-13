5a7eba76f77416513ca11e86dfdaf65f42497de2
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-03-14 00:57:33 +0700

    [REFACTOR] omit v-app for non-page component

--- src/resources/app/components/CourseCard.vue
+++ src/resources/app/components/CourseCard.vue
@@ -1,46 +1,39 @@
 <template>
-  <v-app>
-    <v-card
-      class="mx-auto rounded-3xl"
-      width="253"
-      @click="actionRedirectToCoursePage(courseId)"
-    >
-      <div class="bg-primary h-24 font-semibold text-base text-slate-50">
-        <div class="flex flex-row">
-          <div class="pl-5 pt-5 text-base" id="course-name">
-            {{ courseName }}
-          </div>
-          <button class="flex flex-col items-end space-x-0 space-y-0 pr-1 pt-5">
-            <div class="rotate-90 font-bold">...</div>
-          </button>
+  <v-card
+    class="mx-auto rounded-3xl"
+    width="253"
+    @click="actionRedirectToCoursePage(courseId)"
+  >
+    <div class="bg-primary h-24 font-semibold text-base text-slate-50">
+      <div class="flex flex-row">
+        <div class="pl-5 pt-5 text-base" id="course-name">
+          {{ courseName }}
         </div>
-
-        <div class="flex flex-row-reverse">
-          <v-avatar class="bg-gray z-0 mt-5 mr-5" size="60"></v-avatar>
-        </div>
+        <button class="flex flex-col items-end space-x-0 space-y-0 pr-1 pt-5">
+          <div class="rotate-90 font-bold">...</div>
+        </button>
       </div>
 
-      <div class="m-3">
-        <div class="font-normal text-xs" id="room-tag">Room</div>
-        <div class="font-medium text-sm mb-3" id="classroom">
-          {{ roomNum }}
-        </div>
-        <div class="font-normal text-sm" id="teacher-name">
-          {{ teacherName }}
-        </div>
+      <div class="flex flex-row-reverse">
+        <v-avatar class="bg-gray z-0 mt-5 mr-5" size="60"></v-avatar>
       </div>
-    </v-card>
-  </v-app>
+    </div>
+
+    <div class="m-3">
+      <div class="font-normal text-xs" id="room-tag">Room</div>
+      <div class="font-medium text-sm mb-3" id="classroom">
+        {{ roomNum }}
+      </div>
+      <div class="font-normal text-sm" id="teacher-name">
+        {{ teacherName }}
+      </div>
+    </div>
+  </v-card>
 </template>
 
 <script>
 export default {
-  props: [
-    "courseId",
-    "courseName",
-    "roomNum",
-    "teacherName"
-  ],
+  props: ["courseId", "courseName", "roomNum", "teacherName"],
   methods: {
     actionRedirectToCoursePage(id) {
       window.location.assign(`/course/${id}`);
