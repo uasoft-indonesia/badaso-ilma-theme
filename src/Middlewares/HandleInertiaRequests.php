ef731de52b935efdb8e1a1ae1be39948006f837e
IlmaMannix <70790610+IlmaMannix@users.noreply.github.com>
2022-03-13 17:57:32 +0700

    [REFACTOR] fix code smell

--- src/Middlewares/HandleInertiaRequests.php
+++ src/Middlewares/HandleInertiaRequests.php
@@ -21,7 +21,7 @@ class HandleInertiaRequests extends Middleware
      *
      * @see https://inertiajs.com/asset-versioning
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
     public function version(Request $request)
@@ -34,7 +34,7 @@ class HandleInertiaRequests extends Middleware
      *
      * @see https://inertiajs.com/shared-data
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function share(Request $request)
