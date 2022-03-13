9a7ca9aab1bb41670933da0c5b577257b4942624
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-03-13 15:15:01 +0700

    [RED] test user authentication

--- tests/unit/course/create.spec.js
+++ tests/unit/course/create.spec.js
@@ -1,26 +1,33 @@
 import Vuetify from "vuetify";
+import Vuex from "vuex";
 import { mount, createLocalVue } from "@vue/test-utils";
 
 import { createCourse } from "../../../src/resources/api/course/create";
 import CreateCourse from "  ../../../src/resources/app/pages/course/CreateCourse";
-import store from "../../../src/resources/app/store/store";
 
 Object.defineProperty(window, "location", {
   writable: true,
   value: { assign: jest.fn() },
-}); 
+});
 
 const localVue = createLocalVue();
+localVue.use(Vuex);
 
 jest.mock("../../../src/resources/api/course/create", () => ({
-  createCourse: jest.fn()
+  createCourse: jest.fn(),
 }));
 
 describe("CreateCourse.vue", () => {
   let vuetify;
+  let store;
 
   beforeEach(() => {
     vuetify = new Vuetify();
+    store = new Vuex.Store({
+      state: {
+        isAuthenticated: false,
+      },
+    });
   });
 
   it("should render correct forms", () => {
@@ -38,6 +45,42 @@ describe("CreateCourse.vue", () => {
     expect(wrapper.find("#create-btn").exists()).toBe(true);
   });
 
+  describe("when user is not authenticated", () => {
+    it("should redirect to login page", () => {
+      Object.defineProperty(window, "location", {
+        writable: true,
+        value: { assign: jest.fn() },
+      });
+
+      const wrapper = mount(CreateCourse, {
+        localVue,
+        vuetify,
+        store,
+      });
+
+      expect(window.location.assign).toHaveBeenCalledWith("/login");
+    });
+  });
+
+  describe("when user is authenticated", () => {
+    it("should not redirect anywhere", async () => {
+      Object.defineProperty(window, "location", {
+        writable: true,
+        value: { assign: jest.fn() },
+      });
+
+      store.state.isAuthenticated = true;
+
+      const wrapper = mount(CreateCourse, {
+        localVue,
+        vuetify,
+        store,
+      });
+
+      expect(window.location.assign).not.toHaveBeenCalled();
+    });
+  });
+
   describe("when all fields are filled", () => {
     it("form should be valid", async () => {
       const wrapper = mount(CreateCourse, {
@@ -53,7 +96,7 @@ describe("CreateCourse.vue", () => {
             subject: "Test Subject",
             room: "Test Room",
           },
-        }
+        },
       });
 
       expect(wrapper.vm.form.isValid).toBe(true);
@@ -62,6 +105,11 @@ describe("CreateCourse.vue", () => {
 
   describe("when submitted successfully", () => {
     it("should redirect to course page", async () => {
+      Object.defineProperty(window, "location", {
+        writable: true,
+        value: { assign: jest.fn() },
+      });
+
       const wrapper = mount(CreateCourse, {
         localVue,
         vuetify,
@@ -127,10 +175,15 @@ describe("CreateCourse.vue", () => {
       expect(wrapper.vm.snackbar.isVisible).toBe(true);
       expect(wrapper.vm.snackbar.text).toBe("Test Error");
     });
-  })
+  });
 
   describe("when cancel button is clicked", () => {
     it("should redirect to home page", async () => {
+      Object.defineProperty(window, "location", {
+        writable: true,
+        value: { assign: jest.fn() },
+      });
+
       const wrapper = mount(CreateCourse, {
         localVue,
         vuetify,
