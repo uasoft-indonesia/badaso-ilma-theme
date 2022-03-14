b29c02cf3423f0fe9a3bfdbfeca61d419ccfb943
Muhammad Fathan Muthahhari <fathanmuthahhari@gmail.com>
2022-03-14 21:35:47 +0700

    [REFACTOR] resolve conflicts

--- src/Helpers/Routes.php
+++ src/Helpers/Routes.php
@@ -8,10 +8,6 @@ class Routes
     {
         $controllers = config('badaso-lms-theme.controllers');
 
-<<<<<<< HEAD
-        // if the key is not found, return $key
-=======
->>>>>>> 8e1a5b857480d0421e83220957ebaabbfe45f485
         if (! isset($controllers[$key])) {
             return 'Uasoft\\Badaso\\Theme\\LMSTheme\\Controllers\\'.$key;
         }
