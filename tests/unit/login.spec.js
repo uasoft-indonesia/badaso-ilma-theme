0a325a395d17ab8d68a07780fb88faf7b5cec2f8
nofamex <nsiren517@gmail.com>
2022-03-15 00:45:31 +0700

    [CHORES] Pipeline

--- tests/unit/login.spec.js
+++ tests/unit/login.spec.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import { mount } from "@vue/test-utils";
-import { validate } from "uuid";
-import Login from "../../src/resources/app/pages/login.vue";
-
-describe("Login", () => {
-  describe("when loaded", () => {
-    const wrapper = mount(Login, {
-      data() {
-        return {
-          valid: true,
-          email: "",
-          emailRules: [
-            (v) => !!v || "Email is required",
-            (v) => /.+@.+\..+/.test(v) || "Email must be valid",
-          ],
-          password: "",
-          passwordRules: [(v) => !!v || "Password is required"],
-          remember: false,
-          backgroundImage: "../../assets/undraw_on_the_office_re_cxds.svg",
-        };
-      },
-      methods() {
-        validate(), login();
-      },
-    });
-
-    it("has all of the required elements", () => {
-=======
 import { mount, createLocalVue } from "@vue/test-utils";
 import Vuetify from "vuetify";
 import { validate } from "uuid";
@@ -65,7 +36,6 @@ describe("Login", () => {
         vuetify,
       });
 
->>>>>>> 2e49ac125524bc500fc979301281c3a00c2e4afb
       expect(wrapper.find("#input-email").exists()).toBe(true);
       expect(wrapper.find("#input-password").exists()).toBe(true);
       expect(wrapper.find("#btn-register").exists()).toBe(true);
@@ -76,8 +46,6 @@ describe("Login", () => {
     });
 
     it("test data input for login component", () => {
-<<<<<<< HEAD
-=======
       const wrapper = mount(Login, {
         data() {
           return {
@@ -99,7 +67,6 @@ describe("Login", () => {
         localVue,
         vuetify,
       });
->>>>>>> 2e49ac125524bc500fc979301281c3a00c2e4afb
       wrapper.setData({ email: "wrong email" });
       expect(wrapper.find(".error").exists()).toBeFalsy();
 
