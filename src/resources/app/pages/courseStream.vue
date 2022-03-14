7d4d50731bf1a4d9fc06fd67a6087a43efe5bd69
Darren Ngoh <58223773+darren-ngoh@users.noreply.github.com>
2022-03-15 00:18:49 +0700

    Changed indentation from 4 to 2

--- src/resources/app/pages/courseStream.vue
+++ src/resources/app/pages/courseStream.vue
@@ -1,33 +1,24 @@
 <template>
-    
-    <v-card
-  elevation="2"
-  outlined
-  shaped
-  >
+  <v-card elevation="2" outlined shaped>
     <v-card-text>
       <h2 id="code-title">Class code</h2>
       <div id="class-code">R00M00</div>
-      <v-btn @click="copyCode('R00M00')"
-      id="copy-btn">
-          Copy
-      </v-btn>
-    </v-card-text>  
-
-    </v-card>
+      <v-btn @click="copyCode('R00M00')" id="copy-btn"> Copy </v-btn>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
 export default {
-    methods: {
-        async copyCode(code) {
-            try {
-                await navigator.clipboard.writeText(code);
-                alert('Copied');
-            } catch($e) {
-                alert('Cannot copy');
-            }
-        }
-    }
-}
+  methods: {
+    async copyCode(code) {
+      try {
+        await navigator.clipboard.writeText(code);
+        alert("Copied");
+      } catch ($e) {
+        alert("Cannot copy");
+      }
+    },
+  },
+};
 </script>
