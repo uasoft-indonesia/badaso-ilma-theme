e20c418f0d171469130714b8e7de380626d970cf
Darren Ngoh <58223773+darren-ngoh@users.noreply.github.com>
2022-03-15 00:16:58 +0700

    [REFACTOR] add test for method

--- tests/unit/copyClassCode.spec.js
+++ tests/unit/copyClassCode.spec.js
@@ -1,17 +1,59 @@
-import { mount } from "@vue/test-utils";
+import Vuetify from "vuetify";
+import Vuex from "vuex";
+import { mount, createLocalVue } from "@vue/test-utils";
 import courseStream from "../../src/resources/app/pages/courseStream";
 
-describe("Class Code Widget", () => {
-    describe("when loaded", () => {
-        it("should renders", () => {
-            const wrapper = mount(courseStream, {});
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
-            const findTitle = () => wrapper.find('#code-title')
-            expect(findTitle().exists()).toBe(true);
-            const classCode = () => wrapper.find('#class-code')
-            expect(classCode().exists()).toBe(true);
-            const copyBtn = () => wrapper.find('#copy-btn')
-            expect(copyBtn().exists()).toBe(true);
-        })
-    })
-})
+describe("Class Code Widget", () => {
+  let vuetify;
+  let store;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    store = new Vuex.Store({
+      state: {
+        isAuthenticated: false,
+      },
+    });
+  });
+  describe("when loaded", () => {
+    it("should renders", () => {
+      const wrapper = mount(courseStream, {});
+
+      const findTitle = () => wrapper.find("#code-title");
+      expect(findTitle().exists()).toBe(true);
+      const classCode = () => wrapper.find("#class-code");
+      expect(classCode().exists()).toBe(true);
+      const copyBtn = () => wrapper.find("#copy-btn");
+      expect(copyBtn().exists()).toBe(true);
+
+      
+    });
+  });
+  describe("When click", () => {
+    it("should call function", async () => {
+        window.alert = jest.fn()
+      const wrapper = mount(courseStream, {
+        localVue,
+        vuetify,
+        store,
+        mocks: {
+          window: {
+            alert: () => jest.fn(),
+          },
+        },
+      });
+      global.navigator.clipboard = {
+          writeText:jest.fn()
+      }
+      const copyBtn = () => wrapper.find("#copy-btn");
+      copyBtn().trigger("click");
+      copyBtn().vm.$emit("click");
+      await wrapper.vm.$nextTick();
+      
+      expect(navigator.clipboard.writeText).toHaveBeenCalled();
+    });
+  });
+});
