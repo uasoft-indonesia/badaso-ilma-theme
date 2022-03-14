9f78cc90a224a33097caca7433eab3a2c566daf4
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2022-03-15 00:19:30 +0700

    pass test

--- src/resources/app/components/AddCourseCard.vue
+++ src/resources/app/components/AddCourseCard.vue
@@ -1,43 +1,35 @@
 <template>
-  <v-card
-    class="mx-auto rounded-xl"
-    width="253"
-  >
+  <v-card class="mx-auto rounded-xl" width="253">
     <div
       class="h-48 bg-light font-semibold text-primary flex flex-col items-center"
     >
-      <div 
-        class="text-base mb-5 mt-12"
-        id="add-course-tag"
-      >
-        Add Course
-      </div>
-      <div
-        class="space-x-2"
-      >
-        <button 
+      <div class="text-base mb-5 mt-12" id="add-course-tag">Add Course</div>
+      <div class="space-x-2">
+        <button
           class="bg-white hover:bg-gray w-20 p-1 border border-gray-400 rounded shadow"
           id="create-btn"
           color="error"
+          @click="createCourse"
         >
-          <div class="text-primary font-medium text-sm">
-            Create
-          </div>
+          <div class="text-primary font-medium text-sm">Create</div>
         </button>
         <button
           class="bg-primary hover:bg-secondary w-20 p-1 border border-gray-400 rounded shadow"
           elevation="0"
         >
-          <div class="text-white font-medium text-sm" id="join-btn">
-            Join
-          </div>
+          <div class="text-white font-medium text-sm" id="join-btn">Join</div>
         </button>
       </div>
-    </div>  
+    </div>
   </v-card>
 </template>
 
 <script>
-  export default {
-  }
+export default {
+  methods: {
+    createCourse() {
+      this.$inertia.visit("/course/create");
+    },
+  },
+};
 </script>
