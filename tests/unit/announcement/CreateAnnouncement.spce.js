8c1827d8b86391ab29ba3a00a58049bd326ca36a
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-25 20:09:48 +0700

    [RED] Check if initial create announce form is exist

---
+++ tests/unit/announcement/CreateAnnouncement.spce.js
@@ -0,0 +1,25 @@
+import Vuetify from "vuetify";
+import { mount, createLocalVue } from "@vue/test-utils";
+import CreateAnnouncement from "../../../src/resources/app/components/CreateAnnouncement";
+
+const localVue = createLocalVue();
+
+describe("CreateAnnouncement", () => {
+  let vuetify;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+  });
+
+  describe("when loaded", () => {
+    it("should renders", () => {
+      const wrapper = mount(CreateAnnouncement, {
+        localVue,
+        vuetify,
+      });
+
+      const findInitialForm = wrapper.find("#initial-form");
+      expect(findInitialForm.exists()).toBe(true);
+    });
+  });
+});
