4aa3bf2a7fb770cfde80f34d37a7f5e48ebc326b
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-26 01:05:51 +0700

    [GREEN] Implement required interfaces

---
+++ src/resources/app/components/Container/AnnouncementContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="container">
+    <CreateAnnouncement/>
+    <AnnouncementCard/>
+  </div>
+</template>
+
+<script>
+import CreateAnnouncement from "../CreateAnnouncement";
+import AnnouncementCard from "../AnnouncementCard";
+
+export default {
+  name: "AnnouncementContainer",
+  components: {AnnouncementCard, CreateAnnouncement}
+}
+</script>
+
+<style scoped>
+
+</style>
