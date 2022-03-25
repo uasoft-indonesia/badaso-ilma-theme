2f6c0b06ac98cc38b0e63f896f7defae7a830a0d
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-26 00:11:40 +0700

    [GREEN] Implement all required interfaces

---
+++ src/resources/app/components/AnnouncementCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card
+    class="mx-auto my-3"
+    width="650"
+    outlined
+  >
+    <div id="announcement">
+      <AnnouncementContent
+        :name="'User'"
+        :date="'19 January 2022'"
+        :content="'Test Content'"
+      />
+    </div>
+
+    <!--announcement comments-->
+    <div
+      id="comments"
+      v-if="comments.length !== 0"
+      v-for="comment in comments"
+      v-bind:key="comment.id"
+    >
+      <v-divider></v-divider>
+      <AnnouncementContent
+        :name="comment.name"
+        :date="comment.date"
+        :content="comment.content"
+      />
+    </div>
+  </v-card>
+
+</template>
+
+<script>
+import AnnouncementContent from "./AnnouncementContent";
+
+export default {
+  name: "AnnouncementCard",
+  components: {AnnouncementContent},
+  data() {
+    return {
+      comments: []
+    }
+  }
+}
+</script>
