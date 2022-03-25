0b9a88f9f7772edc28a90106a870610ee6987860
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-25 23:26:42 +0700

    [RED] Check if author is exist

---
+++ tests/unit/announcement/AnnouncementContent.spec.js
@@ -0,0 +1,27 @@
+import Vuetify from "vuetify";
+import { mount, createLocalVue } from "@vue/test-utils";
+import AnnouncementContent from "../../../src/resources/app/components/AnnouncementContent";
+
+const localVue = createLocalVue();
+
+describe("AnnouncementContent", () => {
+  let vuetify;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+  });
+
+  describe("when loaded", () => {
+    describe('and when no comment exist', () => {
+      it("should renders",  () => {
+        const wrapper = mount(AnnouncementContent, {
+          localVue,
+          vuetify,
+        });
+
+        const findAuthor = wrapper.find("#author");
+        expect(findAuthor.exists()).toBe(true);
+      });
+    });
+  });
+});
