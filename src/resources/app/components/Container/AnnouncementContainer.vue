d002b89186dee1e875257a3175d9f3474ffc767c
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-28 16:04:39 +0700

    [CHORES] Wiring with get announcement end point

--- src/resources/app/components/Container/AnnouncementContainer.vue
+++ src/resources/app/components/Container/AnnouncementContainer.vue
@@ -1,16 +1,43 @@
 <template>
   <div id="container">
     <CreateAnnouncement/>
-    <AnnouncementCard/>
+    <div v-for="announcement in announcements" v-bind:key="announcement.id">
+      <AnnouncementCard
+        :content="announcement.content"
+        :created-by="announcement.createdBy"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import CreateAnnouncement from "../CreateAnnouncement";
 import AnnouncementCard from "../AnnouncementCard";
+import api from "../../../api/announcement/GetAnnouncements";
+
+const courseId = window.location.href.split("/")[4];
 
 export default {
   name: "AnnouncementContainer",
+  data() {
+    return {
+      announcements: []
+    }
+  },
+  methods: {
+    async getAnnouncements() {
+      console.log("test")
+      try {
+        const response = await api.GetAnnouncements(courseId);
+        this.announcements = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  mounted() {
+    this.getAnnouncements();
+  },
   components: {AnnouncementCard, CreateAnnouncement}
 }
 </script>
