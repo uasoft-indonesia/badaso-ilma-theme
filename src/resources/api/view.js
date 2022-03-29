554f5e4230e21bbe3d366115f1b49538cb74e7a4
Nabila Khansa <nabila.khansa@gmail.com>
2022-03-29 21:42:27 +0700

    [REFACTOR] api call change

--- src/resources/api/view.js
+++ src/resources/api/view.js
@@ -4,7 +4,7 @@ let apiPrefix = "/badaso-api";
 
 export default {
   async view() {
-    let apiUrl = apiPrefix + "/module/lms/v1/course/view";
+    let apiUrl = apiPrefix + "/module/lms/v1/course/";
     let response = await api.get(apiUrl);
 
     return response;
