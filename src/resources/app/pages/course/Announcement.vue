956b7728eb2b99d1f6dc7d64488d46dae1cd8b9c
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-03-29 18:17:46 +0700

    [GREEN] Announcement page exist on load

--- src/resources/app/pages/course/Announcement.vue
+++ src/resources/app/pages/course/Announcement.vue
@@ -1,11 +1,15 @@
 <template>
-  <v-app>
+  <div id="announcement-container">
     <AnnouncementContainer/>
-  </v-app>
+  </div>
 </template>
 <script>
 import AnnouncementContainer from "../../components/Container/AnnouncementContainer";
+import CourseStream from "../courseStream";
+import AppLayout from "../../components/Layout/AppLayout";
+
 export default {
+  layout: [AppLayout, CourseStream],
   components: {AnnouncementContainer}
 }
 </script>
