1dfb078e6867cbaee975cd15e4665f3ddf6d4e98
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-04-13 20:35:26 +0700

    [RED] Check if title: Create Material is exist

---
+++ tests/unit/CreateClassMaterial.spec.js
@@ -0,0 +1,37 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+
+import {createLocalVue, mount} from "@vue/test-utils";
+import CreateCourseMaterial from "../../src/resources/app/pages/course/CreateCourseMaterial";
+import Vuex from "vuex";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe("CreateCourseMaterial", () => {
+  let vuetify;
+  let store;
+
+  beforeEach(() => {
+    vuetify = new Vuetify();
+    store = new Vuex.Store({
+      state: {
+        isAuthenticated: true,
+      },
+    });
+  });
+
+  describe("when loaded", () => {
+    it("should renders", () => {
+      const wrapper = mount(CreateCourseMaterial, {
+        localVue,
+        vuetify,
+        store,
+      });
+
+      const findTitle = () => wrapper.find("#title");
+      expect(findTitle().exists()).toBe(true);
+      expect(findTitle().text()).toBe("CreateMaterial")
+    })
+  })
+})
