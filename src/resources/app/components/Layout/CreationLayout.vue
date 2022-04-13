60e6024e2e5c42a163741ef2bb85927f19cbe47d
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-04-13 18:11:35 +0700

    [CHORES] Create layout for create/edit topic, material, assignment, quiz

---
+++ src/resources/app/components/Layout/CreationLayout.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app>
+    <v-container>
+      <div class="mt-16">
+        <v-btn
+          color="white"
+          elevation="0"
+          style="padding-left: 0; padding-right: 8px"
+          @click="redirectBackToClasswork()">
+          <v-icon>
+            mdi-chevron-left
+          </v-icon>
+          Back
+        </v-btn>
+        <div
+          class="text-primary text-lg font-medium mb-4 mt-1"
+        >
+          {{pageTitle}}
+        </div>
+        <v-divider color="#06BBD3" class="mb-6"></v-divider>
+        <slot/>
+      </div>
+    </v-container>
+  </v-app>
+
+</template>
+
+<script>
+import AppLayout from "./AppLayout";
+
+export default {
+  layout: [AppLayout],
+  props: {
+    courseId: String,
+    pageTitle: String,
+  },
+  name: "CreationLayout",
+  methods: {
+    redirectBackToClasswork() {
+      this.$inertia.visit(`/course/${this.$props.courseId}/classwork`);
+    },
+  }
+}
+</script>
+
+<style scoped>
+.no-padding{
+  min-width: 0
+}
+</style>
