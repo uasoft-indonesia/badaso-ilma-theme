a98645c919717faf08ebdfbf4fc1c1dd8898414a
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-04-14 08:56:50 +0700

    [GREEN] Implement menu

--- src/resources/app/components/Layout/CreationLayout.vue
+++ src/resources/app/components/Layout/CreationLayout.vue
@@ -14,20 +14,50 @@
           </v-icon>
           Back
         </v-btn>
-        <div class="my-4">
-          <div
-            id="topic-title"
-            v-if="this.$props.topicTitle"
-            class="text-textGray text-md"
-          >
-            {{ topicTitle }}
+        <div class="my-4 d-flex justify-between items-center">
+          <div>
+            <div
+              id="topic-title"
+              v-if="this.$props.topicTitle"
+              class="text-textGray text-md"
+            >
+              {{ topicTitle }}
+            </div>
+            <div
+              id="title"
+              class="text-primary text-lg font-medium"
+            >
+              {{ pageTitle }}
+            </div>
           </div>
-          <div
-            id="title"
-            class="text-primary text-lg font-medium"
+          <v-menu
+            id="menu"
+            v-if="contentType"
+            bottom
+            rights
           >
-            {{ pageTitle }}
-          </div>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                icon
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item link>
+                <v-list-item-title class="w-28 text-sm">
+                  Edit
+                </v-list-item-title>
+              </v-list-item>
+              <v-list-item link>
+                <v-list-item-title class="w-28 text-sm text-error">
+                  Delete
+                </v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
         </div>
         <v-divider id="divider" color="#06BBD3" class="mb-6"></v-divider>
         <slot/>
@@ -45,6 +75,8 @@ export default {
     courseId: String,
     pageTitle: String,
     topicTitle: String,
+    contentType: String,
+    contentId: String,
   },
   name: "CreationLayout",
   methods: {
