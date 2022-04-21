286be318fe1596de854420fac6f47c933f6e0fd0
Work Place <57487697+FXDROS@users.noreply.github.com>
2022-04-22 04:06:13 +0700

    [REFACTOR] Add props dummy data

--- tests/unit/CourseMaterial.spec.js
+++ tests/unit/CourseMaterial.spec.js
@@ -31,7 +31,11 @@ describe("CourseMaterial", () => {
           data() {
             return {
               material: {
+                title: "Test",
                 description: null,
+                topic: {
+                  title: "Topic"
+                }
               }
             }
           }
@@ -50,7 +54,11 @@ describe("CourseMaterial", () => {
           data() {
             return {
               material: {
+                title: "Test",
                 content: "test description",
+                topic: {
+                  title: "Topic"
+                }
               }
             }
           }
