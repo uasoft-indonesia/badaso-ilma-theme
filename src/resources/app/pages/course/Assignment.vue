049afb3f83629ffc238a165a94874e24182f7abc
Ilma Mannix <ilmamannix2013@gmail.com>
2022-05-18 08:38:02 +0700

    [GREEN] implement view assignment

---
+++ src/resources/app/pages/course/Assignment.vue
@@ -0,0 +1,142 @@
+<template>
+  <CreationLayout
+    :courseId="this.$props.courseId"
+    :pageTitle="this.assignment.title"
+    :topicTitle="this.assignment.topic.title"
+    :contentId="this.$props.assignmentId"
+    :teacherId="this.teacherId"
+    contentType="assignment"
+  >
+    <div id="description" v-if="this.assignment.description" class="text-base mb-9">
+      {{ this.assignment.description }}
+    </div>
+    <div
+      v-if="this.assignment.fileUrl"
+      class="flex flex-col justify-center items-center"
+    >
+      <embed :src="this.assignment.fileUrl" width="80%" height="500px" />
+      <a :href="this.assignment.fileUrl" target="_blank" class="mt-2 mb-10">
+        <v-btn color="primary">Download File</v-btn>
+      </a>
+    </div>
+    <div
+      v-if="this.assignment.linkUrl"
+      class="flex flex-col justify-center items-center"
+    >
+      <a :href="this.assignment.linkUrl" target="_blank" class="mt-2 mb-10">
+        {{ this.assignment.linkUrl }}
+      </a>
+    </div>
+    <v-form ref="form" v-model="isValid" class="mt-8">
+      <div class="flex">
+        <v-btn color="primary" elevation="0" class="mr-6" height="56">
+          <v-icon> mdi-paperclip</v-icon>
+        </v-btn>
+        <v-file-input
+          id="file-form"
+          truncate-length="100"
+          outlined
+          accept="image/png, image/jpeg, application/pdf, image/jpg"
+          label="File"
+          prepend-icon=""
+          v-model="file"
+        ></v-file-input>
+      </div>
+      <div class="flex">
+        <v-btn color="primary" elevation="0" class="mr-6" height="56">
+          <v-icon> mdi-link</v-icon>
+        </v-btn>
+        <v-text-field
+          id="link-form"
+          label="Link"
+          outlined
+          v-model="form.link_url"
+        ></v-text-field>
+      </div>
+    </v-form>
+    <div class="text-right mt-7">
+      <v-btn
+        id="cancel-button"
+        color="error"
+        elevation="0"
+        @click="redirectBackToClasswork"
+      >
+        Cancel
+      </v-btn>
+      <v-btn
+        id="create-button"
+        class="ml-4"
+        color="primary"
+        elevation="0"
+        :disabled="!isValid"
+      >
+        Save
+      </v-btn>
+    </div>
+  </CreationLayout>
+</template>
+<script>
+import CreationLayout from "../../components/Layout/CreationLayout";
+import AppLayout from "../../components/Layout/AppLayout";
+import { getCourseAssignmentById } from "../../../api/course/assignment";
+import { courseDetail } from "../../../api/course/detail";
+
+export default {
+  name: "Assignment",
+  components: {CreationLayout},
+  layout: [AppLayout],
+  props: {
+    courseId: String,
+    assignmentId: String,
+  },
+  data(){
+    return {
+      isValid: false,
+      file: null,
+      items: [],
+      assignment: {},
+      teacherId: String,
+      form: {
+        file_url: '',
+        link_url: '',
+      },
+    }
+  },
+  methods: {
+    async getCourseAssignment() {
+      try {
+        let response = await getCourseAssignmentById(this.$props.assignmentId);
+        if (response.data.topic === null) {
+          response.data.topic = { title: "" };
+        }
+        this.assignment = response.data;
+      } catch (error) {
+        await this.$store.dispatch("OPEN_SNACKBAR", "Error getting data");
+      }
+    },
+    async getTeacher(courseId) {
+      try {
+        const response = await courseDetail(courseId);
+        if (response.data.createdBy){
+          this.teacherId = response.data.createdBy
+        }
+      } catch (error) {
+        await this.$store.dispatch("OPEN_SNACKBAR", "Error getting data");
+      }
+    },
+    redirectBackToClasswork() {
+      this.$inertia.visit(`/course/${this.$props.courseId}/classwork`);
+    },
+    getItemValue(item) {
+      return item.id;
+    },
+    getItemText(item) {
+      return item.title;
+    },
+  },
+  created() {
+    this.getCourseAssignment();
+    this.getTeacher(this.$props.courseId);
+  },
+}
+</script>
